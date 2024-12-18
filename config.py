893a43a48a28a8bb005d34a8a2598deacc887000
Raushan Kumar <121452343+raushan22882917@users.noreply.github.com>
2024-12-18 11:50:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
-    GROQ_API_KEY = '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'
+    GROQ_API_KEY = ''
